bd8fe5d751299b73544018c52cd47c276a052969
Qiro Vasilovski <8vasilov@informatik.uni-hamburg.de>
2022-09-17 20:47:16 +0200

    Footer looks good

--- src/components/structure/Footer.jsx
+++ src/components/structure/Footer.jsx
@@ -1,11 +1,54 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 
 function Footer() {
     return (
         <div className="footer-container">
-            <div>
-                <h1 className="text h1">STH</h1>
+            <div className="header-container">
+                <h1 className="text-h1">
+                    Sejdo Marble - SH3 Bitinckë, Bilisht, 7008 Albania
+                </h1>
+
+                <p className="text-p">
+                    <Link className="text-p-link" to="/contact">
+                        Contact us for more!
+                    </Link>
+                </p>
+            </div>
+            <div className="social-icons">
+                <Link
+                    className="social-icon-link"
+                    to={{
+                        pathname: "https://www.facebook.com/MermerSejdo/",
+                    }}
+                    target="_blank"
+                    aria-label="Facebook"
+                >
+                    <FaFacebook />
+                </Link>
+                <Link
+                    className="social-icon-link"
+                    to={{
+                        pathname:
+                            "https://www.instagram.com/naturstein.mermer_sejdo/",
+                    }}
+                    target="_blank"
+                    aria-label="Instagram"
+                >
+                    <FaInstagram />
+                </Link>
+            </div>
+            <div className="privacy-policy-container">
+                <p className="privacy-policy">
+                    <Link className="privacy-policy-link" to="/contact">
+                        Privacy Policy
+                    </Link>
+                </p>
+            </div>
+            <div className="website-rights">
+                SEJDO © 2022: All rights reserved
             </div>
         </div>
     );
