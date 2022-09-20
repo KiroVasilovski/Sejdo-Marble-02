7cc57259b83641aea57391241bfa282f0b883424
Qiro Vasilovski <8vasilov@informatik.uni-hamburg.de>
2022-09-21 00:20:59 +0200

    Created Homepage Slideshow
    Install react-slideshow npm

--- src/components/homepage/Home.jsx
+++ src/components/homepage/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Slideshow from "./Slider";
 
 function Home() {
-    return <h1> I am Homepage </h1>;
+    return (
+        <div>
+            <Slideshow /> <h1> I am Homepage </h1>
+        </div>
+    );
 }
 
 export default Home;
---
+++ src/components/homepage/Slider.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Fade } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+import "./Slider.css";
+
+const Slideshow = () => {
+    const images = [
+        "https://i.imgur.com/IEwQ4UR.jpg",
+        "https://i.imgur.com/5HCcHWf.jpg",
+        "https://i.imgur.com/jcLqSYT.jpg",
+        "https://i.imgur.com/ojjeOU5.jpg",
+        "https://i.imgur.com/dUcCkIj.jpg",
+        "https://i.imgur.com/wBVj4gr.jpg",
+    ];
+
+    return (
+        <div>
+            <Fade>
+                <div className="each-slide">
+                    <div className="image-holder">
+                        <img src={images[0]} />
+                        <p>First Slide</p>
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div className="image-holder">
+                        <img src={images[1]} />
+                        <p>Second Slide</p>
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div className="image-holder">
+                        <img src={images[2]} />
+                        <p>Third Slide</p>
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div className="image-holder">
+                        <img src={images[3]} />
+                        <p>Fr Slide</p>
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div className="image-holder">
+                        <img src={images[4]} />
+                        <p>Fv Slide</p>
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div className="image-holder">
+                        <img src={images[5]} />
+                        <p>Fv Slide</p>
+                    </div>
+                </div>
+            </Fade>
+        </div>
+    );
+};
+
+export default Slideshow;
